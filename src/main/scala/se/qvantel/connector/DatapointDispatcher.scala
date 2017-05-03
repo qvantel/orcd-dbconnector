2e2447efa4a89185246a0dbbc658c43408561054
Max Meldrum <max@meldrum.se>
2017-05-03 10:48:48 +0200

    Refactor

--- src/main/scala/se/qvantel/connector/DatapointDispatcher.scala
+++ src/main/scala/se/qvantel/connector/DatapointDispatcher.scala
@@ -8,19 +8,13 @@ import scala.collection.mutable
 import scala.util.Try
 
 class DatapointDispatcher extends LazyLogging with DispatcherConfig {
-
   var socket = None: Option[Socket]
   var graphiteAddress = None: Option[InetSocketAddress]
   var baos = None: Option[ByteArrayOutputStream]
   var startIntervalDate = 0L
 
+  // Used for unit tests, to disable side effects
   var autoSend : Boolean = true
-
-  // Output metric of how many metrics were send during $timeStampInterval
-  var elementsInBatch = 0
-
-  // Output metric of how many times a netcat/query was sent to graphite.
-  var messagesSent = 0
 
   type CdrCount = Int
   type Destination = String
@@ -46,12 +40,13 @@ class DatapointDispatcher extends LazyLogging with DispatcherConfig {
           case None => logger.error("Graphite address not set")
         }
       }
-      case None => logger.info("Socket has not been initialized")
+      case None => logger.info("Socket did not get initialized")
     }
   }
 
   // If the saved timestamp exceeds $timeStampInterval, it's time to send to the metric database.
-  def isTimeToSendRecords(timestamp : Long): Boolean = (timestamp - startIntervalDate) >= timeStampInterval
+  def isTimeToSendRecords(timestamp : Long): Boolean =
+    (timestamp - startIntervalDate) >= timeStampInterval
 
   def sendMetrics(timeStamp: Long) : Unit = {
    startIntervalDate match {
@@ -75,14 +70,10 @@ class DatapointDispatcher extends LazyLogging with DispatcherConfig {
   }
 
   def append(destination: String, timeStamp: Long): Unit = {
-
     if (!countedRecords.contains(destination)) {
       countedRecords.put(destination, 0)
     }
     countedRecords.put(destination, countedRecords(destination) + 1)
-
-    // Increment output metric
-    elementsInBatch += 1
 
     // See if need to send metrics
     sendMetrics(timeStamp)
@@ -90,7 +81,6 @@ class DatapointDispatcher extends LazyLogging with DispatcherConfig {
 
   def dispatch(ts: Long): Unit = {
     // Socket output stream
-
     val out = getStream match {
       case Left(outputStream) => new PrintStream(outputStream)
       case Right(byteArrayOutputStream) => {
@@ -98,11 +88,6 @@ class DatapointDispatcher extends LazyLogging with DispatcherConfig {
         new PrintStream(baos.get)
       }
     }
-
-    // Log and count messages sent
-    messagesSent += elementsInBatch
-    elementsInBatch = 0
-
     // Send payload
     val payload = countedRecords.map(p => s"${p._1} ${p._2.toString} ${ts} ")
       .mkString("\n")
@@ -110,11 +95,6 @@ class DatapointDispatcher extends LazyLogging with DispatcherConfig {
     out.print(payload)
   }
 
+  def close(): Unit = socket.foreach(_.close())
 
-  def close(): Unit = {
-    socket match {
-      case Some(sock) => sock.close()
-      case None => logger.error("Trying to close socket without it being open")
-    }
-  }
 }

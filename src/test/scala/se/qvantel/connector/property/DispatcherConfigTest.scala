1ff54cd389b86fb0121bfe8b3c7db978d0bf46f9
Johan Bjäreholt <johan@bjareho.lt>
2017-05-10 10:28:50 +0200

    Removed unused config variables and config comments

--- src/test/scala/se/qvantel/connector/property/DispatcherConfigTest.scala
+++ src/test/scala/se/qvantel/connector/property/DispatcherConfigTest.scala
@@ -5,7 +5,6 @@ import org.scalatest.FunSuite
 class DispatcherConfigTest extends FunSuite with DispatcherConfig {
 
   test("Test dispatcher config") {
-    assert(timeout > 0)
     assert(graphiteTimeoutReconnectionMs > 0)
     assert(timeStampInterval > 0)
   }
--- src/test/scala/se/qvantel/connector/property/ProcessingTest.scala
+++ src/test/scala/se/qvantel/connector/property/ProcessingTest.scala
@@ -5,7 +5,6 @@ import org.scalatest.FunSuite
 class ProcessingTest extends FunSuite with ProcessingConfig {
 
   test("That a valid processing config exists") {
-    assert(batchSize > 0 && batchSize <= 10000)
     assert(fetchBatchSize > 0 && fetchBatchSize <= 20000)
     assert(updateInterval > 0 && updateInterval <= 10000)
   }
